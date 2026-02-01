fdd02012c1f9411a2fac5bd07a9f81bf191b18a6
JMFH <HH_JMFH@PROTON.ME>
2026-02-01 11:27:50 -0600

    v2.5.0 - Fix splash screen version string

--- utils.cpp
+++ utils.cpp
@@ -388,7 +388,7 @@ void displayLogo(uint16_t color, int displayTime) {
 
   // Version line
   tft.setTextFont(2);  // Font 2 = 16px
-  String version = "v2.4 - HaleHound Edition";
+  String version = "v2.5.0 - HaleHound Edition";
   int16_t versionWidth = tft.textWidth(version);
   tft.setCursor((screenWidth - versionWidth) / 2, 285);
   tft.print(version);
@@ -401,7 +401,7 @@ void displayLogo(uint16_t color, int displayTime) {
   tft.print(credit);
 
   Serial.println("==========================================");
-  Serial.println("ESP32-DIV v2.4 - HaleHound Edition        ");
+  Serial.println("ESP32-DIV v2.5.0 - HaleHound Edition      ");
   Serial.println("Developed by: HaleHound (JMFH)            ");
   Serial.println("Original by:  CiferTech                   ");
   Serial.println("GitHub:       github.com/JesseCHale       ");
